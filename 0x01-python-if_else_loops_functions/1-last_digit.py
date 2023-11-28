04f7d3ba69b1268337ceacbdfcd6cd07c045e5c2
Ziyanda Nkosi <zeenkosi572@gmail.com>
2023-11-28 13:27:59 +0200

    Task 1 corrections

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,7 +10,7 @@ else:
 
     if last_digit == 0:
         print(f"{message} and is 0")
-    elif last_digti > 5 and last_digit % 10 != 0:
+    elif last_digit > 5 and last_digit % 10 != 0:
         print(f"{message} and is greater than 5")
     else:
         print(f"{message} and is less than 6 and not 0")
