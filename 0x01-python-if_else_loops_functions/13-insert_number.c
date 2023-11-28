03e16879048a3ab762f0a2154c7ab5969980180f
Ziyanda Nkosi <zeenkosi572@gmail.com>
2023-11-28 13:23:29 +0200

    Madatory Tasks completed

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,63 @@
+#include "lists.h"
+
+listint_t *create_node(int n);
+
+/**
+ * insert_node - inserts a node sorted in linked list of ints
+ * @head: double pointer to head of ll. Needed in edge modification
+ * @number: data for new node
+ * Return: pointer to newly created node, NULL if failure
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *cur_node = NULL, *new_node = NULL;
+
+	if (!head)
+		return (NULL);
+	else if (!(*head))
+	{
+		new_node = create_node(number);
+		*head = new_node;
+		return (new_node);
+	}
+	cur_node = *head;
+	while (cur_node)
+	{
+		if (cur_node->n >= number)
+		{
+			new_node = create_node(number);
+			new_node->next = cur_node;
+			*head = new_node;
+			return (new_node);
+		}
+		else if (cur_node->n <= number)
+		{
+			if (!cur_node->next || cur_node->next->n >= number)
+			{
+				new_node = create_node(number);
+				new_node->next = cur_node->next;
+				cur_node->next = new_node;
+				return (cur_node->next);
+			}
+		}
+		cur_node = cur_node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * create_node - creates a new node for ll
+ * @n: data to insert in new node
+ * Return: pointer to newly allocated node
+ */
+listint_t *create_node(int n)
+{
+	listint_t *ret = NULL;
+
+	ret = malloc(sizeof(listint_t));
+	if (!ret)
+		return (NULL);
+	ret->next = NULL;
+	ret->n = n;
+	return (ret);
+}
