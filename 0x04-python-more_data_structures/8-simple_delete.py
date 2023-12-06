f1d0af5fb80b030cf12cd6a94c875cba135417e2
Ziyanda Nkosi <zeenkosi572@gmail.com>
2023-12-06 12:55:41 +0200

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,8 +2,4 @@
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
         del a_dictionary[key]
-    return a_dictionary#!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:
-        del a_dictionary[key]
     return a_dictionary
